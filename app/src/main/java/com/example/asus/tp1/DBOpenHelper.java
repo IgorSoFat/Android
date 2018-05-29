2c41de9628c964668360dee98732bf16650851ac
Robin Dupret <robin.dupret@gmail.com>
2018-05-29 14:55:01 +0200

    Petit ménage au niveau de DBOpenHelper
    Utilisation d'un `StringBuilder` plutôt`que de plusieurs concaténations
    histoire d'avoir un code plus propre et suppression de la méthode
    d'insertion qui est maintenant disponible au niveau du Fragment.

--- app/src/main/java/com/example/asus/tp1/DBOpenHelper.java
+++ app/src/main/java/com/example/asus/tp1/DBOpenHelper.java
@@ -1,30 +1,31 @@
 package com.example.asus.tp1;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DBOpenHelper extends SQLiteOpenHelper{
+public class DBOpenHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "myData.db";
     public static final int DATABASE_VERSION = 1;
 
 
-    public static final String ID = "id";
+    public static final String ID     = "id";
     public static final String PRENOM = "prenom";
-    public static final String NOM = "nom";
+    public static final String NOM    = "nom";
     public static final String NUMERO = "numero";
-    public static final String SEXE = "sexe";
+    public static final String SEXE   = "sexe";
 
     public static final String TABLE_NAME = "Contact";
-    public static final String TABLE_CREATE =
-            "CREATE TABLE "+TABLE_NAME+" ("+
-                    ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                    PRENOM + " TEXT, " +
-                    NOM + " TEXT, " +
-                    NUMERO + " TEXT, "+
-                    SEXE + " TEXT);";
+    public static final String TABLE_CREATE = new StringBuilder("CREATE TABLE ")
+                                                .append(TABLE_NAME)
+                                                .append("(")
+                                                .append(ID).append(" INTEGER PRIMARY KEY AUTOINCREMENT,")
+                                                .append(PRENOM).append(" TEXT,")
+                                                .append(NOM).append(" TEXT,")
+                                                .append(NUMERO).append(" TEXT,")
+                                                .append(SEXE).append(" TEXT);").toString();
+
     public static final String TABLE_DROP = " DROP TABLE IF EXISTS " + TABLE_NAME + ";";
 
     public DBOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -41,17 +42,4 @@ public class DBOpenHelper extends SQLiteOpenHelper{
         db.execSQL(TABLE_DROP);
         onCreate(db);
     }
-
-
-/*
-    public long insert(Contact contact){
-        ContentValues contactToInsert  = new ContentValues();
-        contactToInsert.put("nom",contact.getNom());
-        contactToInsert.put("prenom",contact.getPrenom());
-        contactToInsert.put("numero",contact.getNumero());
-        contactToInsert.put("sexe",contact.getSexe());
-        return .insert(TABLE_NAME, null, contactToInsert);
-    }
-    */
-
 }

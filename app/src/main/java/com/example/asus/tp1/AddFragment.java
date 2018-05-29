768f6629f83b62dcf42e25d32ee154940e7bc43f
Robin Dupret <robin.dupret@gmail.com>
2018-05-29 14:52:06 +0200

    Mise en place de l'ajout des contacts
    Simple mise en place des différents appel à l'API de DBOpenHelper
    et SQLite3 pour permettre l'insertion d'un utilisateur au moment
    de l'envoie du formulaire.
    
    Le petit piège ici est de ne pas spécifier l'ID de l'utilisateur
    dans le `contentValues` car l'id de l'utilisateur étant `null`,
    le cast vers un `int` donnera 0 et créera un fail au niveau de la
    contrainte `UNIQUE` en base.

--- app/src/main/java/com/example/asus/tp1/AddFragment.java
+++ app/src/main/java/com/example/asus/tp1/AddFragment.java
@@ -1,15 +1,125 @@
 package com.example.asus.tp1;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class AddFragment extends Fragment {
+    private SQLiteDatabase db;
+    private DBOpenHelper dbOpenHelper;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.add_tab, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        dbOpenHelper = new DBOpenHelper(
+            this.getContext(),
+            DBOpenHelper.DATABASE_NAME,
+            null,
+            DBOpenHelper.DATABASE_VERSION
+        );
+
+        openDB();
+
+        // +-----------------------------------+
+        // | Gestion de l'envoie du formulaire |
+        // +-----------------------------------+
+        final Button insert = (Button) getActivity().findViewById(R.id.buttonInsert);
+
+        final EditText editNom       = getActivity().findViewById(R.id.editNom);
+        final EditText editPrenom    = getActivity().findViewById(R.id.editPrenom);
+        final EditText editNumero    = getActivity().findViewById(R.id.editNum);
+        final RadioButton radioHomme = getActivity().findViewById(R.id.radioHomme);
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+             public void onClick(View v) {
+                Contact c = new Contact(
+                    editPrenom.getText().toString(),
+                    editNom.getText().toString(),
+                    editNumero.getText().toString(),
+                    radioHomme.isChecked() ? "Homme" : "Femme"
+                );
+
+                // Si l'insertion de l'utilisateur a réussi, on redirige vers la liste
+                // de contacts.
+                if (insertRecord(c) != -1) {
+                    ((MainActivity)getActivity()).viewPager.setCurrentItem(2);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        openDB();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        closeDB();
+    }
+
+    /**
+     * Permet d'avoir accès à la base de données via l'attribut
+     * `db` de la classe.
+     *
+     * @throws SQLiteException
+     */
+    protected void openDB() throws SQLiteException {
+        try {
+            db = dbOpenHelper.getWritableDatabase();
+        } catch (SQLiteException ex) {
+            db = dbOpenHelper.getReadableDatabase();
+        }
+    }
+
+    /**
+     * Permet de fermer la connexion avec la base de données.
+     */
+    protected void closeDB() {
+        db.close();
+    }
+
+    /**
+     * Permet d'insérer une instance de la classe `Contact` dans la base
+     * de données.
+     *
+     * @param c - Le contact à insérer
+     * @return L'id du nouveau contact
+     */
+    protected long insertRecord(Contact c) {
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(DBOpenHelper.PRENOM, c.getPrenom());
+        contentValues.put(DBOpenHelper.NOM,    c.getNom());
+        contentValues.put(DBOpenHelper.NUMERO, c.getNumero());
+        contentValues.put(DBOpenHelper.SEXE,   c.getSexe());
+
+        long rowId = db.insert(DBOpenHelper.TABLE_NAME, null, contentValues);
+
+        if (rowId == -1) {
+            Toast.makeText(this.getContext(), "Erreur lors de la création", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this.getContext(), "Contact créé avec succès", Toast.LENGTH_LONG).show();
+        }
+
+        return rowId;
     }
 }

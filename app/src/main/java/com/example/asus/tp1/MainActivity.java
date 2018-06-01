665da82e0107cd066bdadb4612067a387501d975
Robin Dupret <robin.dupret@gmail.com>
2018-06-01 13:09:56 +0200

    Petit ménage

--- app/src/main/java/com/example/asus/tp1/MainActivity.java
+++ app/src/main/java/com/example/asus/tp1/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.asus.tp1;
 
-import android.app.Activity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     protected ViewPager viewPager;
